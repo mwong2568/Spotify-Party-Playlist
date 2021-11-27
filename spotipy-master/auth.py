a09bcff119fcbcd45818b136272bff5f88aa5e6a
And New <andoryu@Ands-MBP.hsd1.ca.comcast.net>
2021-11-27 15:56:43 -0800

    more comment on auth.py songList function in classes.py user class

--- spotipy-master/auth.py
+++ spotipy-master/auth.py
@@ -9,7 +9,7 @@ redirect_uri = ''
 scope = "user-read-recently-played"
 
 #____Test Variables____
-#client_id = 'bfee299fd5d24332b15f9d9f5bc18def'
+#lient_id = 'bfee299fd5d24332b15f9d9f5bc18def'
 #client_secret = '74aa77871b4b42838130acadeab8ae2c'
 #redirect_uri = 'http://localhost:9000'
 
@@ -23,10 +23,16 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 auth_manager.cache_handler.save_token_to_cache(
             auth_manager.get_access_token(as_dict=False, check_cache=False))
 
-
+#print(sp.current_user())
 #data=sp.track('5laBj6BPcK9UlwxzLp2fS4')
 #print(data['album']['artists'][0]['name'])
 #print(sp.recommendation_genre_seeds())
-#data=sp.current_user_recently_played(limit=2)
-#print(data['items'][1]['track']['album']['artists'][0]['name'])
+
+#data=sp.current_user_recently_played(limit=3)
+
+#for i in range(len(data['items'])):
+#    print(data['items'][i]['track']['album']['artists'][0]['name'])
+
+#print(data['items'][0]['track']['album']['artists'][0]['name'])
 #print(sp.user('4cqen3duqk51gyesbtxvqk9dh'))
+#print(data)
