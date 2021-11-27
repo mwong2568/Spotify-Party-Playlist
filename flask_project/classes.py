51651ed3f123da86c3c1438085a0a16795e03d3d
Ian1092 <iamaris@scu.edu>
2021-11-27 13:03:37 -0800

    Changes to user and song classes

--- flask_project/classes.py
+++ flask_project/classes.py
@@ -1,21 +1,28 @@
 class User:
-    def __init__(self, user, user_id):
-        self.username = user
-        self.id = user_id
-    
+    def __init__(self, user_id, user_secret, user_name):
+        #   authentication
+        self.user_id = user_id
+        self.user_secret = user_secret
+        self.user_name = user_name
+
+        # user data
+        self.song_history = []
+
     def whoami(self):
         # test func
-        print(self.username, self.id)
-    
+        print(self.user_id, self.user_secret, self.user_name)
+
     def get_song_history(self):
         #TODO
+        #for obj in self.list:
+        #    print(obj.get_song_id())
         pass
 
 class Room:
     def __init__(self, room_id):
         self.room_id = room_id
         self.users = []
-    
+
     def create_playlist(type):
         #TODO
         pass
@@ -26,15 +33,13 @@ class Room:
 
 class Song:
     def __init__(self, song_id):
+        #variables
         self.song_id = song_id
-    
-    def get_artist():
-        #TODO 
-        pass
+        self.song_artist = song_artist
+        self.song_genre = song_genre
 
-    def get_genre():
-        #TODO
-        pass
+    def get_song_id(self):
+        return self.song_id
 
 class Playlist:
     def __init__(self, songs, type):
@@ -49,30 +54,30 @@ class Playlist:
         else:
             print('invalid type')
             return
-    
+
     def create_by_artist(self):
-        #TODO 
+        #TODO
         pass
 
     def create_by_genre(self):
-        #TODO 
+        #TODO
         pass
 
 class Infographic:
     def __init__(self, songs, num_users):
         self.songs = songs
         self.num_users = num_users
-    
+
     def create(self):
         if self.num_users > 2:
             self.create_large()
         else:
             self.create_small()
-    
+
     def create_large(self):
         #TODO
         pass
-    
+
     def create_small(self):
         #TODO
         pass
