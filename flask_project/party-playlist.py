f09dfc6a5d3360985363e02afef4328940b7e286
Miles Wong <mileswong2568@gmail.com>
2021-11-30 17:17:07 -0800

    working artist/genre playlist page, added return button

--- flask_project/party-playlist.py
+++ flask_project/party-playlist.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 app.secret_key = 'dfwjhifhaidshjfbgadsikhfbadsihf'
 test_arr = [1,2,3,5,4]
 
-#test_user = User('miles', 1)
-#data = [test_user]
-
 rooms = []
 
 @app.route('/test', methods=['GET','POST'])
@@ -36,26 +33,26 @@ def spotify():
 def home():
     if request.method == 'GET':
         return f"The URL is being accessed directly, go back to login"
+        
     if request.method == 'POST':
         form_data = request.form
         print(form_data)
         client_id = form_data['client_id']
         client_secret = form_data['client_secret']
         name = form_data['name']
-        
-        
-        #print(recently_played)
-        # Change empty list to function
+
         session['client_id'] = client_id
         session['client_secret'] = client_secret
         session['name'] = name
-        # Create a new User object using form_data info
+
         return render_template('home.html',form_data = form_data)
 
 @app.route('/room', methods = ['POST', 'GET'])
 def room():
     if request.method == 'GET':
-        return f"The URL is being accessed directly, go back to login"
+        room_id = session.get('room_id', None)
+        room = [r for r in rooms if r.get_room_id() == room_id][0]
+    
     if request.method == 'POST':
         redirect_uri = 'http://localhost:9000'
         scope = "user-read-recently-played"
@@ -72,14 +69,11 @@ def room():
             auth_manager.get_access_token(as_dict=False, check_cache=False))
         
         recently_played = sp.current_user_recently_played(limit=50)
-        room_id = request.form['room_id']
-        print('room')
-        #print(form_data)
 
-        #User object should be added to room
+        room_id = request.form['room_id']
 
         current_user = User(client_id, client_secret, name, recently_played)
-        print(current_user.whoami())
+
         room = None
         if room_id not in [r.get_room_id() for r in rooms]:
             new_room = Room(room_id)
@@ -91,14 +85,7 @@ def room():
         room.add_user(current_user)
         room.create_playlist()
         session['room_id'] = room_id
-        #print([r.get_room_id() for r in rooms])
-
         
-
-        for user in room.get_users():
-            print(user.whoami())
-        
-
     return render_template('room.html')
     
 @app.route('/playlist-artist')
@@ -111,7 +98,11 @@ def playlist_artist():
 
 @app.route('/playlist-genre')
 def playlist_genre():
-    return render_template('playlist-genre.html')
+    room_id = session.get('room_id', None)
+    room = [r for r in rooms if r.get_room_id() == room_id][0]
+    playlist_id = room.genre_playlist_id
+    link = 'https://open.spotify.com/embed/playlist/' + playlist_id + '?utm_source=generator&theme=0'
+    return render_template('playlist-genre.html', link = link)
 
 @app.route('/infographic')
 def infographic():
